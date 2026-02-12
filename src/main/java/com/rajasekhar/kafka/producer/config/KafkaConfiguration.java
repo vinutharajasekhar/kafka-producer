44a85eed8f23b644b824a855b327faa28d4c083c
Rajasekhar Reddy Kolli Venkata <rajasekharreddy.kvr@gmail.com>
2026-02-12 09:14:26 +0530

    added idempotence props and tested

--- src/main/java/com/rajasekhar/kafka/producer/config/KafkaConfiguration.java
+++ src/main/java/com/rajasekhar/kafka/producer/config/KafkaConfiguration.java
@@ -38,6 +38,12 @@ public class KafkaConfiguration {
 
     @Value("${spring.kafka.producer.properties.request.timeout.ms}")
     private String requestTimeout;
+
+    @Value(("${spring.kafka.producer.properties.enable.idempotence}"))
+    private boolean idempotence;
+
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+    private int maxInFlightRequests;
     @Bean
     NewTopic createTopic() {
         return TopicBuilder.name("product-created-events-topic")
@@ -66,10 +72,9 @@ public class KafkaConfiguration {
         config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
         config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
         config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
-
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, maxInFlightRequests);
         return config;
     }
-
-
 
 } 
